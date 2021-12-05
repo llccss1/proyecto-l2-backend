1ef0ccd2e1ed82766513f4f99120508975305eb5
llccss1 <quintana.lucas@gmail.com>
2021-12-05 19:54:43 -0300

    movierouter

--- src/routes/moviesRouter.js
+++ src/routes/moviesRouter.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const moviesController = require("../controllers/moviesController");
 
 router.get("/", moviesController.getMovies);
-/*
 router.get("/:id", moviesController.getMovieById);
 router.post("/", moviesController.addMovie);
 router.put("/:id", moviesController.updateMovie);
 router.delete("/:id", moviesController.deleteMovie);
-*/
 
 module.exports = router;
